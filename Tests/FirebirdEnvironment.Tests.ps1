94277e3490193ec43bcb56e586a788c24fd0233e
F.D.Castel <fdcastel@gmail.com>
2025-06-19 17:37:57 -0300

    Adds FirebirdEnvironment.Tests.ps1.

---
+++ Tests/FirebirdEnvironment.Tests.ps1
@@ -0,0 +1,50 @@
+Import-Module "$PSScriptRoot/../PSFirebird.psd1" -Force
+
+BeforeDiscovery {
+    $script:FirebirdVersions = @(
+        '3.0.12',
+        '4.0.5',
+        '5.0.2'
+    )
+}
+
+Describe 'New-FirebirdEnvironment' -ForEach $FirebirdVersions {
+    BeforeAll {
+        $script:FirebirdVersion = $_
+
+        # Create a temporary folder for the test files
+        $script:RootFolder = New-Item -ItemType Directory -Path ([System.IO.Path]::GetTempPath()) -Name (New-Guid)
+        $script:TestEnvironmentPath = Join-Path $RootFolder $FirebirdVersion
+    }
+        
+    AfterAll {
+        # Remove the test folder
+        Remove-Item -Path $RootFolder -Recurse -Force -ErrorAction SilentlyContinue
+    }
+
+    BeforeEach {
+        # Ensure the environment folder does not exist before each test
+        if (Test-Path $TestEnvironmentPath) {
+            Remove-Item -Path $TestEnvironmentPath -Recurse -Force -ErrorAction SilentlyContinue
+        }
+    }
+
+    It 'Create a FirebirdEnvironment of the given version' {
+        $TestEnvironmentPath | Should -Not -Exist
+        $env = New-FirebirdEnvironment -Version $FirebirdVersion -Path $TestEnvironmentPath
+        $env | Should -BeOfType FirebirdEnvironment
+        $TestEnvironmentPath | Should -Exist
+
+        $v = $env.Version
+        [semver]::new($v.Major, $v.Minor, $v.Build) | Should -Be $FirebirdVersion
+    }
+
+    It 'Can run gstat tool' {
+        $env = New-FirebirdEnvironment -Version $FirebirdVersion
+
+        $gstatPath = $env.GetGstatPath().Path
+        $result = & $gstatPath 2>&1
+
+        $result | Should -Be 'use gstat -? to get help'
+    }
+}
