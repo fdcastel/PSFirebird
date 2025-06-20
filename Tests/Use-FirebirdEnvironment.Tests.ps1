2cbc8ab3cbdae173c02c0e392ef0e70eb23165a9
F.D.Castel <fdcastel@gmail.com>
2025-06-20 16:03:39 -0300

    Expand Use-FirebirdEnvironment.Tests.ps1.

--- Tests/Use-FirebirdEnvironment.Tests.ps1
+++ Tests/Use-FirebirdEnvironment.Tests.ps1
@@ -3,15 +3,21 @@ Import-Module "$PSScriptRoot/../PSFirebird.psd1" -Force
 Describe 'Use-FirebirdEnvironment' {
     BeforeAll {
         # Create mock environments for testing
-        $script:mockEnv1 = [FirebirdEnvironment]::new(@{
-            Path = 'C:/TestFirebird/3.0'
-            Version = [version]'3.0.12'
-        })
+        $script:mockEnv3 = [FirebirdEnvironment]::new(@{
+                Path    = '/tmp/mock-firebird-3.0.12'
+                Version = [version]'3.0.12'
+            })
 
-        $script:mockEnv2 = [FirebirdEnvironment]::new(@{
-            Path = 'C:/TestFirebird/5.0'
-            Version = [version]'5.0.2'
-        })
+        $script:mockEnv4 = [FirebirdEnvironment]::new(@{
+                Path    = '/tmp/mock-firebird-4.0.5'
+                Version = [version]'4.0.5'
+            })
+
+        $script:mockEnv5 = [FirebirdEnvironment]::new(@{
+                Path    = '/tmp/mock-firebird-5.0.2'
+                Version = [version]'5.0.2'
+            })
+
     }
 
     It 'FirebirdEnvironment::default() throws an error when no context environment is available.' {
@@ -19,37 +25,64 @@ Describe 'Use-FirebirdEnvironment' {
     }
 
     It 'FirebirdEnvironment::default() uses the context environment when available.' {
-        Use-FirebirdEnvironment -Environment $mockEnv2 {
-            $capturedEnv = [FirebirdEnvironment]::default()
-            $capturedEnv | Should -Be $mockEnv2
-        }
-
-        { [FirebirdEnvironment]::default() } | Should -Throw 'No Firebird environment available*'
-    }
-
-    It 'Context environments can be nested.' {
-        Use-FirebirdEnvironment -Environment $mockEnv1 {
-            $outerEnv = [FirebirdEnvironment]::default()
-            $outerEnv | Should -Be $mockEnv1
-
-            Use-FirebirdEnvironment -Environment $mockEnv2 {
-                $innerEnv = [FirebirdEnvironment]::default()
-                $innerEnv | Should -Be $mockEnv2
-            }
-
-            $restoredEnv = [FirebirdEnvironment]::default()
-            $restoredEnv | Should -Be $mockEnv1  # Should be restored
+        Use-FirebirdEnvironment -Environment $mockEnv5 {
+            [FirebirdEnvironment]::default() | Should -Be $mockEnv5
         }
 
         { [FirebirdEnvironment]::default() } | Should -Throw 'No Firebird environment available*'
     }
 
     It 'Use-FirebirdEnvironment accepts pipeline input.' {
-        $mockEnv1 | Use-FirebirdEnvironment -ScriptBlock {
-            $env = [FirebirdEnvironment]::default()
-            $env | Should -Be $mockEnv1
+        $mockEnv4 | Use-FirebirdEnvironment -ScriptBlock {
+            [FirebirdEnvironment]::default() | Should -Be $mockEnv4
         }
 
         { [FirebirdEnvironment]::default() } | Should -Throw 'No Firebird environment available*'
     }
+
+    It 'Context environments can be nested.' {
+        Use-FirebirdEnvironment -Environment $mockEnv3 {
+            [FirebirdEnvironment]::default() | Should -Be $mockEnv3
+
+            Use-FirebirdEnvironment -Environment $mockEnv4 {
+                [FirebirdEnvironment]::default() | Should -Be $mockEnv4
+
+                Use-FirebirdEnvironment -Environment $mockEnv5 {
+                    [FirebirdEnvironment]::default() | Should -Be $mockEnv5
+                }
+
+                [FirebirdEnvironment]::default() | Should -Be $mockEnv4
+            }
+
+            [FirebirdEnvironment]::default() | Should -Be $mockEnv3
+        }
+
+        { [FirebirdEnvironment]::default() } | Should -Throw 'No Firebird environment available*'
+    }
+
+    It 'Nested context environments can overlap.' {
+        Use-FirebirdEnvironment -Environment $mockEnv3 {
+            [FirebirdEnvironment]::default() | Should -Be $mockEnv3
+
+            Use-FirebirdEnvironment -Environment $mockEnv4 {
+                [FirebirdEnvironment]::default() | Should -Be $mockEnv4
+
+                Use-FirebirdEnvironment -Environment $mockEnv3 {
+                    [FirebirdEnvironment]::default() | Should -Be $mockEnv3
+                }
+
+                [FirebirdEnvironment]::default() | Should -Be $mockEnv4
+
+                Use-FirebirdEnvironment -Environment $mockEnv5 {
+                    [FirebirdEnvironment]::default() | Should -Be $mockEnv5
+                }
+
+                [FirebirdEnvironment]::default() | Should -Be $mockEnv4
+            }
+
+            [FirebirdEnvironment]::default() | Should -Be $mockEnv3
+        }
+
+        { [FirebirdEnvironment]::default() } | Should -Throw 'No Firebird environment available*'
+    }    
 }
